03709d7f2c7bf946a7a1120278692b4302f18e45
Marcos Oliveira <marcosoliveirasamcro@gmail.com>
2019-06-20 18:50:26 -0300

    Eventos

--- ajax.js
+++ ajax.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $("#inicio").click(function(){
 		$(".load").load("inicio.html");
 	});
+	
 	$("#evento").click(function(){
 		$(".load").load("evento.html");
 	});
